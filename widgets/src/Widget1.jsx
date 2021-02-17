611dc59963918e8ae87e347d45ada5c8567a5570
Shawn <shdsouza@seek.com.au>
2021-02-17 17:09:51 +1100

    first commit

---
+++ widgets/src/Widget1.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div
+      style={{
+        fontSize: "xx-large",
+        fontWeight: "bold",
+        color: "darkgreen",
+      }}
+    >
+      I'm widget21 - The Built
+    </div>
+  );
+};
